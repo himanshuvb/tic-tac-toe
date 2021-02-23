5db0a96af84b6e0baab7c506a26551b83d79cdb1
himanshuvb <71548679+himanshuvb@users.noreply.github.com>
2021-02-23 11:55:46 +0530

    s
    s

---
+++ vidembed/app/src/main/java/com/example/vidembed/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.vidembed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView name = findViewById(R.id.videoView);
+        name.setVideoPath("android.resource://" + getPackageName() + "/"  + R.raw.urvid);
+        MediaController mediac = new MediaController(this);
+        name.setMediaController(mediac);
+        mediac.setAnchorView(name);
+        name.start();
+    }
+}
